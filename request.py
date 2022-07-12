1c6cd3035de1501ce40082d1cbe8cd9d12edac0b
EvanIRWU <eisaac181@g.rwu.edu>
2022-07-12 13:19:08 -0400

    Added functions

--- request.py
+++ request.py
@@ -7,6 +7,27 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def get_title(text, host):
+	title = re.search('(?<=<title>).+?(?=</title>)', text, re.DOTALL).group().strip()
+	return title
+def get_request(host):
+	try:
+		r = requests.get(f'https://{host}', verify=False)
+		title = get_title(r.text, host)
+		print(f"Found: {host} | Title: {title}")
+	except requests.exceptions.SSLError:
+		r = requests.get(f'http://{host}', verify=False)
+		title = get_title(r.text, host)
+		print(f"Found: {host} | Title: {title}")
+	except requests.RequestException:
+		print(f"Not Reachable: {host}")
+		return
+
+def get_hosts(file):
+	for host in file_content:
+		if not host.startswith("#"):
+			host = host.strip()
+			get_request(host)
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-f', '--file', required=True, type=argparse.FileType('r'), help='Input a file to be parsed.')
@@ -16,12 +37,16 @@ args = parser.parse_args()
 if args.file:
 	file_content = args.file.readlines()
 
-for host in file_content:
-	host = host.strip()
-	try:
-		r = requests.get(f'https://{host}', verify=False)
-	except requests.exceptions.SSLError:
-		r = requests.get(f'http://{host}', verify=False)
-	title = re.search('(?<=<title>).+?(?=</title>)', r.text, re.DOTALL).group().strip()
-	print("Title: " + title)
-	print(f"{r.text} \n\n" + "-"*50 + "\n")
+# for host in file_content:
+# 	if not host.startswith("#"):
+# 		host = host.strip()
+# 		try:
+# 			r = requests.get(f'https://{host}', verify=False)
+# 		except requests.exceptions.SSLError:
+# 			r = requests.get(f'http://{host}', verify=False)
+# 		except requests.RequestException:
+# 			print(f"Not Reachable: {host}")
+# 		title = get_title()
+# 	# print(f"{r.text} \n\n" + "-"*50 + "\n")
+
+get_hosts(file_content)
